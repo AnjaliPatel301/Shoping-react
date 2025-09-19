99561f434643fc0be434f7770e71d7b3065325f2
Anjali <sj5065070@gmail.com>
2025-09-19 06:39:13 +0530

    Add files via upload

---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from '../features/counterSlice'
+
+
+
+export const store=configureStore({
+    reducer:{
+     cardItem:cartReducer,
+    }
+})
